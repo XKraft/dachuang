5407952e711a2acd89d719be737a37e8645f9426
XKraft <2628178027@qq.com>
2022-04-30 20:57:16 +0800

    bug

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ int main()
     mavlink_status_t status;
     unsigned char BufSend[MAVLINK_MAX_PACKET_LEN];
     int BufSendLen = 0;
-    int chan = 0;
 
     printf("hello pi!\n");
 
@@ -67,7 +66,7 @@ int main()
     // {
     //     cout << "Serial Write successfully! MAVLINK_MSG_ID_SET_ATTITUDE_TARGET" << endl;
     // }
-    mavlink_msg_command_long_pack_chan(100, 200, chan, &_msg, 1, 1, MAV_CMD_SET_MESSAGE_INTERVAL, 0, MAVLINK_MSG_ID_ATTITUDE, 500, 0, 0, 0, 0, 1);
+    mavlink_msg_command_long_pack(100, 200, &_msg, 1, MAV_COMP_ID_ALL, MAV_CMD_SET_MESSAGE_INTERVAL, 0, MAVLINK_MSG_ID_ATTITUDE, 500, 0, 0, 0, 0, 1);
     BufSendLen = mavlink_msg_to_send_buffer(BufSend, &_msg);
     printf_packed_msg(BufSend, BufSendLen);
     if(write(fd, BufSend, BufSendLen))
@@ -75,7 +74,7 @@ int main()
         cout << "Serial Write successfully! MAVLINK_MSG_ID_SET_ATTITUDE_TARGET" << endl;
     }
     //mavlink_msg_request_data_stream_pack(1, 1, &_msg, 1, 1, MAVLINK_MSG_ID_ATTITUDE, 5, 1);
-    mavlink_msg_request_data_stream_pack_chan(100, 200, chan, &_msg, 1, 1, MAVLINK_MSG_ID_ATTITUDE, 500, 1);
+    mavlink_msg_request_data_stream_pack(100, 200, &_msg, 1, MAV_COMP_ID_ALL, MAVLINK_MSG_ID_ATTITUDE, 500, 1);
 	BufSendLen = mavlink_msg_to_send_buffer(BufSend, &_msg);
     printf_packed_msg(BufSend, BufSendLen);
     if(write(fd, BufSend, BufSendLen))
