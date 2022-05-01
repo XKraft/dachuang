7ff7aaace4d80ebb276280fb5ffc17f984741119
XKraft <2628178027@qq.com>
2022-05-01 17:53:57 +0800

    Qt

---
+++ QtDemo/build-QtMavlinkDemo-unknown-Debug/CMakeFiles/QtMavlinkDemo.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/dachuang/QtDemo/build-QtMavlinkDemo-unknown-Debug/QtMavlinkDemo_autogen/mocs_compilation.cpp" "/home/pi/dachuang/QtDemo/build-QtMavlinkDemo-unknown-Debug/CMakeFiles/QtMavlinkDemo.dir/QtMavlinkDemo_autogen/mocs_compilation.cpp.o"
+  "/home/pi/dachuang/QtDemo/QtMavlinkDemo/main.cpp" "/home/pi/dachuang/QtDemo/build-QtMavlinkDemo-unknown-Debug/CMakeFiles/QtMavlinkDemo.dir/main.cpp.o"
+  "/home/pi/dachuang/QtDemo/QtMavlinkDemo/mainwindow.cpp" "/home/pi/dachuang/QtDemo/build-QtMavlinkDemo-unknown-Debug/CMakeFiles/QtMavlinkDemo.dir/mainwindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/pi/dachuang/QtDemo/QtMavlinkDemo"
+  "QtMavlinkDemo_autogen/include"
+  "/usr/include/arm-linux-gnueabihf/qt5"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtWidgets"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtGui"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtCore"
+  "/usr/lib/arm-linux-gnueabihf/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ QtDemo/build-QtMavlinkDemo-unknown-Debug/CMakeFiles/QtMavlinkDemo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtMavlinkDemo_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QtMavlinkDemo_autogen.dir/ParseCache.txt"
+  "QtMavlinkDemo_autogen"
+  "CMakeFiles/QtMavlinkDemo.dir/QtMavlinkDemo_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/QtMavlinkDemo.dir/main.cpp.o"
+  "CMakeFiles/QtMavlinkDemo.dir/mainwindow.cpp.o"
+  "QtMavlinkDemo"
+  "QtMavlinkDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtMavlinkDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
